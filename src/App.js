6d168a854a5eb1e5a0c7b6553bd52d906967a11e
FlorianPoirier <florianpoirier1999@gmail.com>
2024-01-17 19:42:07 +0900

    Test on fetch, clean up

--- src/App.js
+++ src/App.js
@@ -1,23 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import axios from "axios";
+import React, { useState } from "react";
 
 function App() {
+
+  const baseURL = "https://openapi.naver.com/apis/n2mt/translate";
+
+  const [text, setText] = useState("");
+
+
+  const handleTraduction = () => {
+
+    const urlEncodee = encodeURIComponent(text);
+    const tokenClientID = process.env.REACT_APP_CLIENT_ID;
+    const tokenClientSecret = process.env.REACT_APP_CLIENT_SECRET;
+
+
+    const postParams = `source=fr&target=kr&text=${urlEncodee}`;
+
+
+    axios.post(baseURL, postParams, {
+      params: {
+        "X-Naver-Client-Id": tokenClientID,
+        "X-Naver-Client-Secret": tokenClientSecret,
+        'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
+        'Access-Control-Request-Method': 'POST',
+        'Access-Control-Request-Headers': 'X-Naver-Client-Id, X-Naver-Client-Secret, Content-Type',
+      },
+    })
+    .then(response => {
+      console.log(response.data);
+    })
+    .catch(error => {
+      console.error('Erreur lors de la requête : ', error);
+    });
+
+    
+    console.log(urlEncodee);
+
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Papago React App</h1>
+
+      <input placeholder="Please write here" onChange={e => setText(e.target.value)} ></input>
+      <button name="traduire" onClick={handleTraduction}>Traduire</button>
+
     </div>
   );
 }
